f867de24fed07f24818cf1cd5fc10d1c9db2e499
Rubait-islam-jami <146682245+Rubait-islam-jami@users.noreply.github.com>
2024-11-08 00:01:53 +0600

    add

---
+++ category/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render,redirect
+from .forms import CategoryForm
+
+def create_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('show_tasks')
+   
+    return render(request,'create_category.html',{'form':form})
